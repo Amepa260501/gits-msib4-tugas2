98d8127ba92f103f72b9e345755f86163defaf21
Amepa260501 <ianpras234@gmail.com>
2023-02-27 14:21:46 +0700

    revisi

--- database/db_freelance.sql
+++ database/db_freelance.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2021 at 10:15 AM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 8.0.2
+-- Waktu pembuatan: 27 Feb 2023 pada 08.17
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contacts`
+-- Struktur dari tabel `contacts`
 --
 
 CREATE TABLE `contacts` (
@@ -33,21 +33,21 @@ CREATE TABLE `contacts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `contacts`
+-- Dumping data untuk tabel `contacts`
 --
 
 INSERT INTO `contacts` (`meta_field`, `meta_value`) VALUES
-('mobile', '0123456789'),
-('email', 'info@sampple.com'),
-('facebook', 'https://facebook.com/profile'),
+('mobile', '081358098102'),
+('email', 'ianpras234@gmail.com'),
+('facebook', 'https://www.facebook.com/photo/?fbid=1681072865625300&set=a.238465483219386'),
 ('twitter', ''),
 ('linkin', ''),
-('address', 'Sample Address, Sample, Sample, 12345');
+('address', 'perum gka');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `education`
+-- Struktur dari tabel `education`
 --
 
 CREATE TABLE `education` (
@@ -61,18 +61,18 @@ CREATE TABLE `education` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `education`
+-- Dumping data untuk tabel `education`
 --
 
 INSERT INTO `education` (`id`, `school`, `degree`, `month`, `year`, `description`, `order_by`) VALUES
-(1, 'Sample School', 'Sample', 'April', 2017, '<p><span style=\"color: rgb(0, 0, 0); font-family: \"Open Sans\", Arial, sans-serif; text-align: justify;\"><span style=\"font-weight: bolder;\">Lorem ipsum dolor sit amet,</span> consectetur adipiscing elit. Quisque malesuada, odio ullamcorper ornare pellentesque, orci quam consectetur urna, sed fringilla nunc lorem lacinia quam. Ut pellentesque luctus mi vitae vestibulum. Vivamus scelerisque congue turpis, vel rutrum felis ultricies ac. Duis vitae ligula pellentesque erat fermentum mattis. Ut fringilla blandit est sit amet malesuada. Mauris ultrices interdum tellus nec tincidunt. Nulla malesuada sem lorem. Pellentesque blandit augue eu mi iaculis faucibus. Vestibulum in nisl at turpis bibendum efficitur. Integer dapibus volutpat nisl eget lobortis. Nam sit amet scelerisque felis. Praesent euismod quis eros et facilisis. Vivamus vitae nibh vitae nunc dapibus placerat. Duis ac accumsan enim, at semper tortor. Nunc aliquet augue eu diam semper sodales. Maecenas pulvinar dignissim ex, vel lacinia massa consectetur ut.</span></p><p><br></p>', 0),
-(2, 'School 2', 'Degree 2', 'March', 2018, '<p><span style=\"color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: justify;\">Sed dui diam, aliquet vel porttitor non, placerat vitae ligula. Donec ut neque at massa accumsan volutpat vitae vitae augue. Morbi dapibus finibus nulla, vitae ultricies lectus iaculis vitae. Morbi maximus vel justo ut consequat. Fusce ut ex feugiat, pulvinar velit sit amet, cursus lorem. Aliquam ullamcorper tempor commodo. Etiam faucibus diam sed arcu vehicula, blandit accumsan nulla placerat.</span><br></p>', 0),
-(3, 'Sample School 33', 'Degree 3', 'April', 2019, '&lt;p&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &quot;Open Sans&quot;, Arial, sans-serif; text-align: justify;&quot;&gt;Etiam viverra sem sit amet dapibus imperdiet. Integer vitae eros ex. Cras ac nunc eleifend, malesuada ligula iaculis, posuere massa. Donec congue tincidunt vehicula. Pellentesque in sem est. Cras venenatis eros eget ipsum blandit, et vulputate ipsum rhoncus. Donec euismod non lacus eu venenatis. Nam magna velit, fringilla sed commodo non, accumsan at elit. Suspendisse sagittis purus ex, sit amet mollis sem vehicula vitae. Vivamus consectetur faucibus libero, efficitur feugiat nibh tempus sed.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;', 0);
+(1, 'Universitas Dinamika', 'S1 ', 'September', 2020, '&lt;p&gt;&lt;font color=&quot;#000000&quot;&gt;Sekarang saya sedang menempuh jenjang S1 Sistem Informasi yang dimana saya sekarang menduduki semester 6&lt;/font&gt;&lt;/p&gt;', 0),
+(2, 'SMK Semen Gresik', 'SMK', 'March', 2019, '&lt;p&gt;&lt;font color=&quot;#000000&quot;&gt;Pada saat SMK saya mengambil jurusan RPL dan disana saya melanjutkan hobi saya yaitu basket dan disana saya mengikuti ekskul basket dan paskibraka.&amp;nbsp;&lt;/font&gt;&lt;/p&gt;', 0),
+(3, 'SMP Negeri 2 Kebomas', 'SMP', 'April', 2016, '&lt;p&gt;&lt;font color=&quot;#000000&quot;&gt;Saya memasuki jenjang SMP pada umur 14 yang dimana saya sebelumnya bersekolah di SD Randuagung dan pada saat smp saya mengikuti ekskul basket dan band&lt;/font&gt;&lt;/p&gt;', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `project`
+-- Struktur dari tabel `project`
 --
 
 CREATE TABLE `project` (
@@ -85,16 +85,16 @@ CREATE TABLE `project` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `project`
+-- Dumping data untuk tabel `project`
 --
 
 INSERT INTO `project` (`id`, `name`, `summary`, `description`, `banner`, `client`) VALUES
-(1, 'Sample Project 101', 'Sed dui diam, aliquet vel porttitor non, placerat vitae ligula. Donec ut neque at massa accumsan volutpat vitae vitae augue. Morbi dapibus finibus nulla, vitae ultricies lectus iaculis vitae. Morbi maximus vel justo ut consequat. Fusce ut ex feugiat, pulvinar velit sit amet, cursus lorem. Aliquam ullamcorper tempor commodo. Etiam faucibus diam sed arcu vehicula, blandit accumsan nulla placerat.', '&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px; text-align: justify; color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif;&quot;&gt;Curabitur venenatis tortor semper finibus congue. Praesent eget tellus ac libero euismod pulvinar sed tincidunt odio. Cras sed viverra orci. Suspendisse cursus faucibus augue, sed feugiat ante commodo vitae. Praesent ut tempus neque. Donec ac ultricies orci, sed egestas nunc. Nunc lacinia, tortor sit amet elementum cursus, ante erat dictum lorem, ut feugiat sapien mi quis nulla. Nunc diam erat, lobortis nec posuere in, tempus non dui. Phasellus eleifend est at neque luctus, in placerat dolor pretium. Integer sagittis lacinia placerat. Nulla in dolor dapibus purus pharetra consectetur ac et tortor. Morbi blandit viverra ipsum, at lobortis odio lacinia tempus. Maecenas ac lobortis nisi. Suspendisse potenti.&lt;/p&gt;&lt;p style=&quot;margin-right: 0px; margin-bottom: 15px; margin-left: 0px; padding: 0px; text-align: justify; color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif;&quot;&gt;Etiam viverra sem sit amet dapibus imperdiet. Integer vitae eros ex. Cras ac nunc eleifend, malesuada ligula iaculis, posuere massa. Donec congue tincidunt vehicula. Pellentesque in sem est. Cras venenatis eros eget ipsum blandit, et vulputate ipsum rhoncus. Donec euismod non lacus eu venenatis. Nam magna velit, fringilla sed commodo non, accumsan at elit. Suspendisse sagittis purus ex, sit amet mollis sem vehicula vitae. Vivamus consectetur faucibus libero, efficitur feugiat nibh tempus sed.&lt;/p&gt;', 'uploads/1619158020_cts_qr_card.png', 'Client 101');
+(2, 'Aliftyan Meirsyah Prasetyo', 'Dimana saya menggarap sebuah aplikasi Perpustakaan\r\n\r\n\r\n\r\n', 'Pada saat saya SMK saya ditugas kan oleh guru saya untuk membuat sebuah aplikasi sederhana dan saya membuat aplikasi perpustakaan.&lt;br&gt;', 'uploads/1677250200_(20+) Facebook — Mozilla Firefox 8_8_2022 6_26_44 PM.png', 'SMK Semen Gresik');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `system_info`
+-- Struktur dari tabel `system_info`
 --
 
 CREATE TABLE `system_info` (
@@ -104,22 +104,22 @@ CREATE TABLE `system_info` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `system_info`
+-- Dumping data untuk tabel `system_info`
 --
 
 INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
-(1, 'name', 'Freelance/Portfolio Website Creator'),
-(2, 'address', 'Philippines'),
+(1, 'name', 'Portofolio Saya'),
+(2, 'address', 'INDONESIA'),
 (3, 'contact', '+1234567890'),
-(4, 'email', 'info@sample.com'),
+(4, 'email', 'ianpras234@gmail.com'),
 (6, 'short_name', 'My Website'),
 (9, 'logo', 'uploads/1616118180_aclc.jpg'),
-(11, 'welcome_message', 'I\'m a Web Developer creating awesome and effective Web Applications for companies of all sizes around the globe. Let\'s start scrolling and learn more about me.');
+(11, 'welcome_message', 'Perkenalkan saya progamer full stack web yang dimana saya telah melakukan ini baru 1 tahun dan sekarang saya berkulaih di Universitas Dinamika yang dimana mengambil jurusan Sistem Informasi');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -135,16 +135,17 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `date_added`, `date_updated`) VALUES
-(1, 'John', 'Smith', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/1619140500_avatar.png', NULL, '2021-01-20 14:02:37', '2021-04-23 15:14:05');
+(1, 'Aliftyan Meirsyah ', 'Prasetyo', 'admin2', '01488dd067097083f80f32761c0ad802', 'uploads/1677426480_(20+) Facebook — Mozilla Firefox 8_8_2022 6_26_44 PM.png', NULL, '2021-01-20 14:02:37', '2023-02-26 22:48:17'),
+(2, 'John', 'Smith', 'admin', '123456', 'uploads/1619140500_avatar.png', NULL, '2021-01-20 14:02:37', '2021-04-23 15:14:05');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `work`
+-- Struktur dari tabel `work`
 --
 
 CREATE TABLE `work` (
@@ -158,76 +159,76 @@ CREATE TABLE `work` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `work`
+-- Dumping data untuk tabel `work`
 --
 
 INSERT INTO `work` (`id`, `company`, `position`, `started`, `ended`, `description`, `order_by`) VALUES
-(1, 'Company 101', 'Web Developer', 'March_2014', 'July_2017', '&lt;p&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; text-align: justify;&quot;&gt;Curabitur venenatis tortor semper finibus congue. Praesent eget tellus ac libero euismod pulvinar sed tincidunt odio. Cras sed viverra orci. Suspendisse cursus faucibus augue, sed feugiat ante commodo vitae. Praesent ut tempus neque. Donec ac ultricies orci, sed egestas nunc. Nunc lacinia, tortor sit amet elementum cursus, ante erat dictum lorem, ut feugiat sapien mi quis nulla. Nunc diam erat, lobortis nec posuere in, tempus non dui. Phasellus eleifend est at neque luctus, in placerat dolor pretium. Integer sagittis lacinia placerat. Nulla in dolor dapibus purus pharetra consectetur ac et tortor. Morbi blandit viverra ipsum, at lobortis odio lacinia tempus. Maecenas ac lobortis nisi. Suspendisse potenti.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;', 0);
+(1, 'Polres Gresik', 'Humas', 'January_2019', 'April_2018', '&lt;p&gt;&lt;font color=&quot;#000000&quot;&gt;Dimana pada saat saya SMK saya mengikuti program magang dari sekolah dan saya diminta sekolah untuk pertama kali magang di Polres Gresik&lt;/font&gt;&lt;/p&gt;', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `education`
+-- Indeks untuk tabel `education`
 --
 ALTER TABLE `education`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `project`
+-- Indeks untuk tabel `project`
 --
 ALTER TABLE `project`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `system_info`
+-- Indeks untuk tabel `system_info`
 --
 ALTER TABLE `system_info`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `work`
+-- Indeks untuk tabel `work`
 --
 ALTER TABLE `work`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `education`
+-- AUTO_INCREMENT untuk tabel `education`
 --
 ALTER TABLE `education`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `project`
+-- AUTO_INCREMENT untuk tabel `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `system_info`
+-- AUTO_INCREMENT untuk tabel `system_info`
 --
 ALTER TABLE `system_info`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `work`
+-- AUTO_INCREMENT untuk tabel `work`
 --
 ALTER TABLE `work`
   MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
